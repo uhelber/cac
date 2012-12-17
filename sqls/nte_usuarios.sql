be84f9cc0dc3cb747dbf323ae66070b2b9b6106e
uhelber <uhelber@gmail.com>
2012-12-17 09:02:03 -0300

    17/12/2012

---
+++ sqls/nte_usuarios.sql
@@ -0,0 +1,67 @@
+CREATE DATABASE  IF NOT EXISTS `nte` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `nte`;
+-- MySQL dump 10.13  Distrib 5.5.16, for Win32 (x86)
+--
+-- Host: localhost    Database: nte
+-- ------------------------------------------------------
+-- Server version	5.5.28-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `usuarios`
+--
+
+DROP TABLE IF EXISTS `usuarios`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `usuarios` (
+  `idusuarios` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(45) NOT NULL,
+  `sobrenome` varchar(45) DEFAULT NULL,
+  `setor` varchar(45) DEFAULT NULL,
+  `datanascimento` date DEFAULT NULL,
+  `datacadastro` date DEFAULT NULL,
+  `cadastrador` int(11) DEFAULT NULL,
+  `telefone` varchar(45) DEFAULT NULL,
+  `matricula` varchar(45) DEFAULT NULL,
+  `usuario` varchar(45) NOT NULL,
+  `senha` varchar(45) NOT NULL,
+  `permissao` int(11) NOT NULL,
+  PRIMARY KEY (`idusuarios`),
+  KEY `fk_idx` (`permissao`),
+  KEY `fk_usuarios_usuarios_idx` (`cadastrador`),
+  CONSTRAINT `fk_usuarios_permissoes` FOREIGN KEY (`permissao`) REFERENCES `permissoes` (`idpermissao`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_usuarios_usuarios` FOREIGN KEY (`cadastrador`) REFERENCES `usuarios` (`idusuarios`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usuarios`
+--
+
+LOCK TABLES `usuarios` WRITE;
+/*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
+INSERT INTO `usuarios` VALUES (1,'Administrador',NULL,NULL,NULL,NULL,1,NULL,NULL,'admim','admin',3),(2,'Uhelber','Lima da Costa','Proinfo',NULL,NULL,1,NULL,NULL,'uhelber','lcosta',2),(3,'Giodano Bruno','Patrício Leal','Proinfo',NULL,NULL,2,NULL,NULL,'bruno','123',2),(4,'Maria do Socorro','Albino',NULL,NULL,NULL,3,NULL,NULL,'malbino','001',1);
+/*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2012-12-06 17:18:37
---
+++ src/java/cac/db/Chamado.java
@@ -0,0 +1,108 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cac.db;
+
+
+/**
+ *
+ * @author UhelberC
+ */
+public class Chamado {
+    private Integer idchamado;
+    private String cidade;
+    private String bairro;
+    private String escola;
+    private String contato;
+    private String telefone;
+    private Status status;
+    private String descricao;
+    private Integer abertopor;
+    private String dataabertura;
+
+    public Chamado() {
+    }
+
+    public Integer getIdchamado() {
+        return idchamado;
+    }
+
+    public void setIdchamado(Integer idchamado) {
+        this.idchamado = idchamado;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getEscola() {
+        return escola;
+    }
+
+    public void setEscola(String escola) {
+        this.escola = escola;
+    }
+
+    public String getContato() {
+        return contato;
+    }
+
+    public void setContato(String contato) {
+        this.contato = contato;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Integer getAbertopor() {
+        return abertopor;
+    }
+
+    public void setAbertopor(Integer abertopor) {
+        this.abertopor = abertopor;
+    }
+
+    public String getDataabertura() {
+        return dataabertura;
+    }
+
+    public void setDataabertura(String dataabertura) {
+        this.dataabertura = dataabertura;
+    }
+
+    
+}
